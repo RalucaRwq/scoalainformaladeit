f38d47910e981845d6b282d57eca49be14d8b4da
RalucaRwq <ciulavur@yahoo.com>
2021-12-13 00:05:07 +0200

    This is my project.

---
+++ proiect/jobs/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from jobs import views
+
+app_name = 'jobs'
+
+urlpatterns = [
+    path('add_job/', views.CreateJobView.as_view(), name='add'),
+    path('<int:pk>/update_job/', views.UpdateJobView.as_view(), name='update'),
+    path('list/', views.ListJobView.as_view(), name='list'),
+    ]
