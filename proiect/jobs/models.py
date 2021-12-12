cade197318f5affba99da08a5450804ec46b3150
RalucaRwq <ciulavur@yahoo.com>
2021-12-12 22:24:48 +0200

    My project

--- proiect/jobs/models.py
+++ proiect/jobs/models.py
@@ -2,12 +2,14 @@ from django.db import models
 
 # Create your models here.
 
+
 class Job(models.Model):
+
     name = models.CharField(max_length=100)
     url = models.CharField(max_length=100)
     description = models.TextField()
     customer = models.ForeignKey('aplicatie2.Companies', on_delete=models.CASCADE)
     active = models.IntegerField(default=1)
 
-# Se realizeaza Create, Read, Update, Delete  (CRUD) pt modulul de Jobs.
-# trb sa avem o validare in care sa nu existe job cu nume si customer identic   
+    def __str__(self):
+        return f"{self.name} {self.url} {self.description} {self.customer} {self.active}"
